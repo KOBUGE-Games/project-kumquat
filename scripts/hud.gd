ce4e2f628e341d23288e2fa1f43c1dcfd7aad039
Alket Rexhepi <alketii@gmail.com>
2015-12-06 16:45:28 +0100

    added tower placement detector, 2 fixes needed

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,6 +6,11 @@ var health
 var health_current = 1000
 var budget_current = 1000
 var placeTower = false
+var level_offset_x = 0
+var level_offset_y = 0
+var tile_size = 32 # FIXME , get it from level node
+var level_size_h = 22 * tile_size # FIXME tile h count * tile size
+var level_size_v = 18 * tile_size # FIXME tile h count * tile size
 
 func _ready():
 	budget = get_node("Budget")
@@ -14,6 +19,8 @@ func _ready():
 	health = get_node("health_label")
 	health.set_text("Health: "+str(health_current))
 	tower_desc.hide()
+	level_offset_x = get_node("/root/Game/Level").get_pos().x
+	level_offset_y = get_node("/root/Game/Level").get_pos().y
 	set_process_input(true)
 	
 	
@@ -31,8 +38,14 @@ func updateHealth(amount):
 	health.set_text("Health: "+str(health_current))
 
 func _input(ev):
-	if placeTower and ev.type == InputEvent.MOUSE_MOTION:
+	if placeTower and ev.type == InputEvent.MOUSE_MOTION and ev.pos.x > level_offset_x and ev.pos.x < level_size_h and ev.pos.y > level_offset_y and ev.pos.y < level_size_v:
 		get_node("cursor_placeholder").set_pos(ev.pos)
+		print(get_node("/root/Game/Level").tiles[Vector2(floor((ev.pos.x-level_offset_x)/tile_size),floor((ev.pos.y+level_offset_y)/tile_size))].type == preload("level.gd").Tile.TILE_BUILDABLE)
+		if get_node("/root/Game/Level").tiles[Vector2(floor((ev.pos.x-level_offset_x)/tile_size),floor((ev.pos.y+level_offset_y)/tile_size))].type == preload("level.gd").Tile.TILE_BUILDABLE:
+			get_node("cursor_placeholder").set_frame(1)
+		else:
+			get_node("cursor_placeholder").set_frame(0)
+
 
 func _on_cancel_pressed():
 	placeTower = false
