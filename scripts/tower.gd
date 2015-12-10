fef53f7b821d283708812ffaa4ff4a358004226a
Alket Rexhepi <alketii@gmail.com>
2015-12-10 20:51:53 +0100

    added a basic machine gun with laser xD

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -53,6 +53,7 @@ func attack():
 		target.hp -= damage
 		attack_indicator.set_scale(Vector2(attack_indicator.get_scale().x, target.get_pos().distance_to(get_pos())/global.TILE_SIZE))
 		attack_indicator.set_rot(target.get_pos().angle_to_point(get_pos()))
+		get_node("rotator").set_rot(target.get_pos().angle_to_point(get_pos()))
 		attack_indicator.show()
 		animation_player.play("attack")
 	else:
