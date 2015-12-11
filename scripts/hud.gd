c3667d11f898a8f1ee5c4b95a6e1de7a4d130604
Rémi Verschelde <rverschelde@gmail.com>
2015-12-11 18:26:16 +0100

    Fix some logic issues in hud
    - a boolean is needed for tower placement mode to prevent processing input events while carried_tower is freed
    - fix mouse button echo in input
    - drop test in tower_build_mode as carried_tower != null but it is a "previously freed instance"

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -11,6 +11,7 @@ var health
 var budget_current = 1000
 var health_current = 1000
 var last_transaction = 0
+var tower_placement = false
 var carried_tower = null
 var level_offset = Vector2()
 
@@ -31,7 +32,7 @@ func _ready():
 	set_process_input(true)
 
 func _input(ev):
-	if carried_tower and ev.pos.x > level_offset.x and ev.pos.x < global.LEVEL_SIZE.x \
+	if tower_placement and carried_tower and ev.pos.x > level_offset.x and ev.pos.x < global.LEVEL_SIZE.x \
 			and ev.pos.y > level_offset.y and ev.pos.y < global.LEVEL_SIZE.y:
 		if ev.type == InputEvent.MOUSE_MOTION:
 			var tile_pos = level.get_node("tilemap_tower").world_to_map(ev.pos - level_offset)
@@ -42,12 +43,13 @@ func _input(ev):
 			else:
 				carried_tower.get_node("sprite").set_modulate(Color(1.0, 0.3, 0.3)) # Non buildable, red
 		
-		elif ev.type == InputEvent.MOUSE_BUTTON and !ev.is_echo():
+		elif ev.type == InputEvent.MOUSE_BUTTON and ev.is_pressed() and !ev.is_echo():
 			if ev.button_index == BUTTON_LEFT:
 				# Place a tower
 				var tile_pos = level.get_node("tilemap_tower").world_to_map(ev.pos - level_offset)
 				if level.tiles[tile_pos].type == level.Tile.TILE_BUILDABLE and !level.tiles[tile_pos].has_tower:
 					# Stop the tower placing behaviour and "drop" the carried tower
+					tower_placement = false
 					carried_tower.set_carried(false)
 					carried_tower = null # Stop referencing this tower in the hud
 					# FIXME: Check if we want to keep it on to place several towers (tower costs have then to be updated)
@@ -72,6 +74,7 @@ func update_health(amount):
 	health.set_text("Health: " + str(health_current))
 
 func cancel_tower_placement():
+	tower_placement = false
 	carried_tower.queue_free()
 	budget_current += last_transaction
 	budget.set_text("Budget: " + str(budget_current))
@@ -79,11 +82,9 @@ func cancel_tower_placement():
 ### Signals ###
 
 func tower_build_mode(tower_scene, price):
-	if carried_tower != null: # Should not happen...
-		carried_tower.free()
-	
 	if budget_current >= price:
 		update_budget(-price)
+		tower_placement = true
 		carried_tower = tower_scene.instance()
 		level.add_child(carried_tower)
 		carried_tower.set_carried(true)
