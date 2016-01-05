b400512057cacbe65f39753812c198675ce28dd6
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-01-05 23:24:02 +0200

    Change directory structure to match set standards
    Close #17

---
+++ main/buttons/btn_tower.gd
@@ -0,0 +1,48 @@
+extends Button
+
+### Variables ###
+
+# Nodes
+var global
+var hud # The HUD node
+var tooltip # The tower tooltip
+
+# Characteristics
+export var tower_name = "Unnamed"
+export var tower_scene_name = "tower1"
+export var tower_damage = 100
+export var tower_range = 100
+export var tower_reload = 1
+export var tower_price = 100
+var tower_scene
+
+### Callbacks ###
+
+func _ready():
+	global = get_node("/root/global")
+	hud = global.hud
+	tooltip = hud.get_node("tower_tooltip")
+
+	tower_scene = load("res://towers/" + tower_scene_name + "/" + tower_scene_name + ".xscn")
+	get_node("icon").set_texture(load("res://towers/" + tower_scene_name + "/" + tower_scene_name + ".png"))
+	set_text(tower_name)
+	
+	if hud.budget_current < tower_price:
+		set_disabled(true)
+
+### Signals ###
+
+func _on_btn_tower_mouse_enter():
+	tooltip.set_pos(get_pos() + Vector2(get_size().x + 10, get_parent().get_pos().y))
+	tooltip.get_node("name").set_text(tower_name)
+	tooltip.get_node("damage").set_text("Damage: " + str(tower_damage))
+	tooltip.get_node("range").set_text("Range: " + str(tower_range))
+	tooltip.get_node("reload").set_text("Reload: " + str(tower_reload))
+	tooltip.get_node("price").set_text("Price: " + str(tower_price))
+	tooltip.show()
+
+func _on_btn_tower_mouse_exit():
+	tooltip.hide()
+
+func _on_btn_tower_pressed():
+	hud.tower_build_mode(tower_scene, tower_price)
---
+++ main/buttons/btn_upgrade.gd
@@ -0,0 +1,39 @@
+extends Button
+
+### Variables ###
+
+# Nodes
+var global
+var hud # The HUD node
+var tooltip # The tower tooltip
+
+# Characteristics
+export var upgrade_price = 100
+export var upgrade_scene = preload("res://towers/drop_upgrade/drop_upgrade.tscn")
+
+### Callbacks ###
+
+func _ready():
+	global = get_node("/root/global")
+	hud = global.hud
+	tooltip = hud.get_node("tower_tooltip")
+	
+	if hud.budget_current < upgrade_price:
+		set_disabled(true)
+
+### Signals ###
+
+func _on_btn_tower_mouse_enter():
+	tooltip.set_pos(get_pos() + Vector2(get_size().x + 10, get_parent().get_pos().y))
+	tooltip.get_node("name").set_text("Upgrade")
+	tooltip.get_node("damage").set_text("")
+	tooltip.get_node("range").set_text("")
+	tooltip.get_node("reload").set_text("")
+	tooltip.get_node("price").set_text("Price: " + str(upgrade_price))
+	tooltip.show()
+
+func _on_btn_tower_mouse_exit():
+	tooltip.hide()
+
+func _on_btn_tower_pressed():
+	hud.tower_upgrade_mode(upgrade_scene, upgrade_price)
