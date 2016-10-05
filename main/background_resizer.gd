63d57eb3a9feb8373173fb6186ec09139dbddb6b
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-10-05 22:25:47 +0300

    Fix background position when zooming in

--- main/background_resizer.gd
+++ main/background_resizer.gd
@@ -8,5 +8,5 @@ func _ready():
 	viewport.connect("size_changed", self, "update_values")
 
 func update_values():
-	texture.set_size(viewport.get_rect().size / viewport.get_canvas_transform().get_scale())
-	set_scroll_base_scale(viewport.get_canvas_transform().get_scale())
+	texture.set_size(viewport.get_rect().size / viewport.get_canvas_transform().get_scale() + Vector2(10,10))
+	set_scroll_base_scale(Vector2(1,1) / viewport.get_canvas_transform().get_scale())
