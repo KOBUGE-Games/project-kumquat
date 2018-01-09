6f8ff66005c98cab624c826076101c8d2b54d0dd
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2018-01-09 18:04:08 +0200

    Fix a bug where missles would timeout too early

--- towers/tower_types/tower_single_target_missile.gd
+++ towers/tower_types/tower_single_target_missile.gd
@@ -14,7 +14,7 @@ func _ready():
 func perform_attack():
 	var direction = Vector2(0, 1).rotated(target.get_pos().angle_to_point(tower.get_pos()))
 	var missile = missile_scene.instance()
-	missile.setup(direction, deal_damage_funcref, reach)
+	missile.setup(direction, deal_damage_funcref, reach + tower.reach_add)
 	missile.set_pos(tower.get_pos())
 	
 	tower.level.add_child(missile)
